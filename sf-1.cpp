aa50d06a1b3cee887f229a4cf7fedffe6a2fe13f
wblxwz <2394559659@qq.com>
2022-06-02 22:05:57 +0800

    First push

---
+++ sf-1.cpp
@@ -0,0 +1,1077 @@
+﻿#include <iostream>
+#include <thread>
+#include <limits>
+#include <Windows.h>
+#include<sql.h>
+#include<sqlext.h>
+#include<sqltypes.h>
+#include <fstream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+SQLRETURN ret;
+SQLHENV henv;
+SQLHDBC hdbc;
+SQLHSTMT hstmt;
+
+//按照习惯常量值大写
+
+
+#define ROWS 3
+#define SIZE 10
+
+//using namespace std;
+
+//编译器优化
+//（显式)代码移动
+//将需要多次使用的循环条件函数写在循环外
+
+//内联函数 告诉编译器最好在调用出展开该函数代码
+
+/*循环展开（编译器会自动优化，正常写循环即可）
+并行累积 （利用流水线，浮点类型手动）
+重新结合变换 （通过括号变换累积顺序，浮点类型手动）*/
+/*三目运算符更快是因为CPU指令流水线性质
+//相当于展开了代码，使用条件数据传送而不是条件分支传送
+避免了分支预测错误的惩罚*/
+
+
+//inline void print(const int* a, const size_t& n)
+//{
+//	int i;
+//	for (i = 0; i < n - 1; i += 2)
+//	{
+//		//cout << a[i] << " ";
+//		cout << a[i] << " " << a[i + 1] << " ";
+//	}
+//	if (i < n)
+//		cout << a[n - 1];
+//	cout << endl;
+//}
+
+//const引用可以引用立即数 普通的引用不能引用立即数
+template<typename T, typename U>
+inline void print(T* a, const U& n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << a[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+template<typename T>
+inline void swap(T& a, T& b)
+{
+	T t = a;
+	a = b;
+	b = t;
+}
+
+//功能：二进制加法
+void add_Bin(int* a, int* b, const size_t& n)
+{
+	int carry = 0;//进位初始化
+	//[]运算符需要传入size_t类型(64位系统下是64位，但是int64位系统下还是32位4字节)
+	//size_t是无符号整型
+	int* c = new int[n + 1];
+	for (size_t i = n; i > 0; --i)
+	{
+		c[i] = (a[i - 1] + b[i - 1] + carry) % 2;
+		carry = (a[i - 1] + b[i - 1] + carry) / 2;
+	}
+	c[0] = carry;
+	print(c, n + 1);
+	delete[]c;
+}
+
+//功能：排序算法
+//插入排序（类似扑克牌，分成两部分，遍历无序的向有序中插入）O(n^2)
+void insert_Sort(int* a, const size_t& n)
+{
+	for (int i = 1; i < n; ++i)
+	{
+		int key = a[i];
+		int j = i - 1;
+		while (j >= 0 && a[j] > key)
+		{
+			a[j + 1] = a[j];
+			--j;
+		}
+		a[j + 1] = key;
+	}
+}
+//选择排序 O(n^2)
+void select_Sort(int* a, const size_t& n)
+{
+	for (int i = 0; i < n - 1; ++i)
+	{
+		int min = i;
+		for (int j = i + 1; j < n; ++j)
+			if (a[j] < a[min])
+				min = j;
+		swap(a[i], a[min]);
+	}
+}
+//冒泡排序 O(n^2)
+void maoPao(int* a, const size_t& n)
+{
+	for (int i = 0; i < n - 1; ++i)
+		for (int j = 0; j < n - i - 1; ++j)
+			if (a[j] > a[j + 1])
+				swap(a[j], a[j + 1]);
+}
+//归并排序算法 O(nlog₂n)
+void merge(int* a, const int start, const int end, int* b)
+{
+	int left_length = (end - start + 1) / 2 + 1;
+	int left_index = start;
+	int right_index = start + left_length;
+	int result_index = start;
+	while (left_index < start + left_length && right_index < end + 1)
+	{
+		if (a[left_index] <= a[right_index])
+		{
+			b[result_index++] = a[left_index++];
+		}
+		else
+		{
+			b[result_index++] = a[right_index++];
+		}
+	}
+	while (left_index < start + left_length)
+	{
+		b[result_index++] = a[left_index++];
+	}
+	while (right_index < end + 1)
+	{
+		b[result_index++] = a[right_index++];
+	}
+}
+//右值表达式不能作为递归时的引用（非常量引用只能用左值）
+void merge_Sort(int* a, const int start, const int end, int* b)
+{
+	if (end == start)
+	{
+		return;
+	}
+	else if (end - start == 1)
+	{
+		if (a[start] > a[end])
+			swap(a[start], a[end]);
+	}
+	else
+	{
+		merge_Sort(a, start, (end - start + 1) / 2 + start, b);
+		merge_Sort(a, (end - start + 1) / 2 + 1, end, b);
+		merge(a, start, end, b);
+	}
+}
+
+//最大子数组
+class Maxarray
+{
+public:
+	Maxarray() = default;
+	Maxarray(const int& min, const int& max, const int& s) :min_index(min), max_index(max), sum(s)
+	{ }
+	inline int getSum() const
+	{
+		return this->sum;
+	}
+	~Maxarray() = default;
+private:
+	int min_index, max_index, sum;
+};
+
+Maxarray find_CrossMaxArray(int* a, const int& low, const int& mid, const int& high)
+{
+	int left_sum = -INT_MAX;
+	int sum = 0;
+	int max_left = 0;
+	for (int i = mid; i >= low; --i)
+	{
+		sum += a[i];
+		if (sum > left_sum)
+		{
+			left_sum = sum;
+			max_left = i;
+		}
+	}
+	int right_sum = -INT_MAX;
+	sum = 0;
+	int max_right = 0;
+	for (int i = mid + 1; i <= high; ++i)
+	{
+		sum += a[i];
+		if (sum > right_sum)
+		{
+			right_sum = sum;
+			max_right = i;
+		}
+	}
+	return Maxarray(max_left, max_right, left_sum + right_sum);
+}
+
+Maxarray find_MaxArray(int* a, const int& low, const int& high)
+{
+	if (low == high)
+		return Maxarray(low, high, a[low]);
+	else
+	{
+		int mid = (low + high) / 2;
+		Maxarray m1 = find_MaxArray(a, low, mid);
+		Maxarray m2 = find_MaxArray(a, mid + 1, high);
+		Maxarray m3 = find_CrossMaxArray(a, low, mid, high);
+		if (m1.getSum() > m2.getSum() && m1.getSum() > m3.getSum())
+			return m1;
+		else if (m2.getSum() > m1.getSum() && m2.getSum() > m3.getSum())
+			return m2;
+		else
+			return m3;
+	}
+}
+
+//矩阵乘法(假定矩阵为n*n)
+class Matrix
+{
+public:
+	Matrix()
+	{
+		rows = ROWS;
+		matrix = new int* [rows];
+		for (int i = 0; i < rows; ++i)
+			matrix[i] = new int[rows];
+		std::cout << "default" << std::endl;
+	}
+	Matrix(const int& r)//假定矩阵为n*n
+	{
+		rows = r;
+		//cols = c;
+		matrix = new int* [rows];
+		for (int i = 0; i < r; ++i)
+			matrix[i] = new int[rows];
+		for (int i = 0; i < r; ++i)
+		{
+			for (int j = 0; j < r; ++j)
+			{
+				int x = 0;
+				std::cin >> x;
+				matrix[i][j] = x;
+			}
+		}
+		std::cout << "OK" << std::endl;
+	}
+	void print() const
+	{
+		for (int i = 0; i < rows; ++i)
+		{
+			for (int j = 0; j < rows; ++j)
+				std::cout << matrix[i][j] << " ";
+			std::cout << std::endl;
+		}
+	}
+	const int getRows() const
+	{
+		return this->rows;
+	}
+	int** getMatrix()
+	{
+		return this->matrix;
+	}
+private:
+	int rows = 0;
+	//int cols = 0;
+	//动态创建二维数组
+	//动态内存分配指的是可以手动释放,返回第一个字节的地址
+	int** matrix = new int* [rows];
+};
+
+//O(n^3)朴素算法一般够用
+//矩阵乘法朴素分治和strassen算法可以以后再看，比较复杂
+Matrix mulitiply(Matrix& a, Matrix& b)
+{
+	int n = a.getRows();
+	Matrix c;
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+		{
+			c.getMatrix()[i][j] = 0;
+			for (int k = 0; k < n; ++k)
+				c.getMatrix()[i][j] += a.getMatrix()[i][k] * b.getMatrix()[k][j];
+		}
+	return c;
+}
+
+//多线程测试
+
+
+/*寻找最大最小值，先设第一个数为最值，遍历数组，不断更新初始值*/
+//至少要做n-1次比较
+int maxValue(int* a, const int& n)
+{
+	int max = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (a[i] > a[max])
+			max = i;
+	}
+	return a[max];
+}
+
+struct Value
+{
+	int max, min;
+};
+
+/*同时寻找最大值和最小值*/
+/*最大比较次数为3⌊n/2⌋*/
+inline Value minMaxValue(int* a, const int& n)
+{
+	int max = 0, min = 0;
+	Value v;
+	if (n % 2 == 0)
+	{
+		max = a[0] > a[1] ? a[0] : a[1];
+		min = a[0] > a[1] ? a[1] : a[0];
+	}
+	else
+	{
+		max = a[0];
+		min = a[0];
+	}
+	for (int i = 1; i < n - 1; i += 2)
+	{
+		if (a[i] >= a[i + 1])
+		{
+			if (a[i] > max)
+			{
+				max = a[i];
+			}
+			if (a[i + 1] < min)
+			{
+				min = a[i + 1];
+			}
+		}
+		else
+		{
+			if (a[i + 1] > max)
+			{
+				max = a[i + 1];
+			}
+			if (a[i] < min)
+			{
+				min = a[i];
+			}
+		}
+	}
+	if (a[n - 1] < min)
+		min = a[n - 1];
+	if (a[n - 1] > max)
+		max = a[n - 1];
+	v.max = max;
+	v.min = min;
+	return v;
+}
+
+
+/*随机算法*/
+//按优先级随机排列数组
+void randomArray(int* a, const int& n)
+{
+	srand(unsigned(time(0)));
+	int* p = new int[n];
+	for (int i = 0; i < n; ++i)
+	{
+		p[i] = rand() % 100;
+	}
+	print(p, 6);
+	for (int i = 0; i < n - 1; ++i)
+	{
+		for (int j = 0; j < n - 1 - i; ++j)
+			if (p[j] > p[j + 1])
+			{
+				swap(a[j], a[j + 1]);
+				swap(p[j], p[j + 1]);
+			}
+	}
+	delete[]p;
+}
+//原址随机排列数组
+void randomInPlace(int* a, const size_t& n)
+{
+	srand(unsigned(time(0)));
+	for (int i = 0; i < n; ++i)
+	{
+		swap(a[i], a[rand() % n]);
+	}
+}
+
+//在线雇用问题（平衡最小化面试次数和最大化雇用质量）
+//k = n/e时至少有1/e的概率取得最好值(e = 2.7)
+int onLine(int* score, const int& n, const int& k)
+{
+	int best = INT_MIN;
+	for (int i = 0; i < k; ++i)
+	{
+		if (score[i] > best)
+			best = score[i];
+	}
+	for (int i = k; i < n; ++i)
+	{
+		if (score[i] > best)
+			return i;
+	}
+	return n;
+}
+
+
+/*树的父结点、左孩子和右孩子
+给定结点为i，父结点为i/2（向下取整），左孩子为2i，右孩子为2i+1
+*/
+inline int Parent(int& i)
+{
+	return i / 2;
+}
+
+inline int Left(int& i)
+{
+	return i * 2;
+}
+
+inline int Right(int& i)
+{
+	return i * 2 + 1;
+}
+
+/*包含n个元素的堆的高度为lgn*/
+//堆近似完全二叉树
+struct Heap
+{
+	int n = 6;
+	int* a;
+};
+
+/*维护最大堆的性质,i为根结点的下标*/
+void max_Heapify(Heap& h, int i)
+{
+	int largest = i;
+	int l = Left(i);
+	int r = Right(i);
+	if (l < h.n && h.a[l] > h.a[i])
+		largest = l;
+	else
+		largest = i;
+	if (r < h.n && h.a[r] > h.a[largest])
+		largest = r;
+	if (largest != i)
+	{
+		swap(h.a[i], h.a[largest]);
+		max_Heapify(h, largest);
+	}
+}
+
+/*建最大堆*/
+/*堆数组的子数组[n/2,n/2+1,...,n-1]都是叶子结点*/
+void buildMaxHeap(Heap& h)
+{
+	//都是根节点
+	for (int i = h.n / 2 - 1; i >= 0; --i)
+	{
+		max_Heapify(h, i);
+	}
+}
+
+/*堆排序 O(nlogn)*/
+size_t heapSort(Heap& h)
+{
+	buildMaxHeap(h);
+	for (int i = h.n - 1; i > 0; --i)
+	{
+		swap(h.a[0], h.a[i]);
+		--h.n;
+		max_Heapify(h, 0);
+
+	}
+	return h.n;
+}
+
+/*最大优先队列*/
+int heapMax(Heap& h)//返回队列中最大键字的元素
+{
+	return h.a[0];
+}
+
+int heapExtractMax(Heap& h)//去掉并返回具有最大键字的元素
+{
+	if (h.n < 1)
+	{
+		std::cout << "error" << std::endl;
+		return 0;
+	}
+	int max = h.a[0];
+	h.a[0] = h.a[h.n - 1];
+	--h.n;
+	max_Heapify(h, 0);
+	return max;
+}
+
+void heapIncreaseKey(Heap& h, int x, int k)//将元素x的关键字值增加到k(赋值)
+{
+	if (k < h.a[x])
+	{
+		std::cout << "error";
+		return;
+	}
+	h.a[x] = k;
+	while (x > 0 && h.a[Parent(x)] < h.a[x])
+	{
+		swap(h.a[x], h.a[Parent(x)]);
+		x = Parent(x);
+	}
+}
+
+void maxHeapInsert(Heap& h, int key)//插入一个新元素
+{
+	++h.n;
+	h.a[h.n - 1] = INT_MIN;
+	heapIncreaseKey(h, h.n - 1, key);
+}
+
+/*快速排序，实际排序中最好的选择，因为平均性能非常好 O(nlgn)*/
+int Partition(int* a, int p, int r)
+{
+
+	int x = a[r];
+	int i = p - 1;
+	for (int j = p; j <= r - 1; ++j)
+	{
+		if (a[j] <= x)
+		{
+			++i;
+			swap(a[i], a[j]);
+		}
+	}
+	swap(a[i + 1], a[r]);
+	return i + 1;
+}
+
+void quickSort(int* a, int p, int r)
+{
+	if (p < r)
+	{
+		int q = Partition(a, p, r);
+		quickSort(a, p, q - 1);
+		quickSort(a, q + 1, r);
+	}
+}
+
+int Random(const int a, const int b)
+{
+	srand(unsigned(time(0)));
+	return rand() % (b - a + 1) + a;
+}
+
+/*随机化版本的快速排序，在处理大数据输入时效果更好*/
+int randomPartition(int* a, int p, int r)
+{
+	int i = Random(p, r);
+	swap(a[i], a[r]);
+	return Partition(a, p, r);
+}
+
+void randomQuickSort(int* a, int p, int r)
+{
+	if (p < r)
+	{
+		int q = randomPartition(a, p, r);
+		randomQuickSort(a, p, q - 1);
+		randomQuickSort(a, q + 1, r);
+	}
+}
+
+/*选择第i个顺序统计量（即数组中第i个最小值）*/
+/*假设所有元素是互异的，O(n)*/
+int randomSelect(int* a, int p, int r, int i, const int& n)
+{
+	--i;
+	if (p == r)
+		return a[p];
+	int q = randomPartition(a, p, r);
+	int k = q - p + 1;
+	if (i == k)
+		return a[q];
+	else if (i < k)
+		return randomSelect(a, p, q - 1, i, n);
+	else
+		return randomSelect(a, q + 1, r, i, n);
+}
+
+
+//计数排序 O(k+n)
+//有条件 输入的所有数小于等于k 对输入元素做假设
+//是稳定的排序（相等的数排序完成后不改变其顺序，比如a2 a4都是3，排完后a2还在a4前面）
+void countSort(int* a, const int k, const int n)
+{
+	int* c = new int[k];
+	int* b = new int[n];
+	for (int i = 0; i < k; ++i)
+	{
+		c[i] = 0;
+	}
+	for (int j = 0; j < n; ++j)
+	{
+		++c[a[j]];
+	}
+	for (int i = 1; i < k; ++i)
+	{
+		c[i] += c[i - 1];
+	}
+	for (int j = n - 1; j >= 0; --j)
+	{
+		b[c[a[j]] - 1] = a[j];
+		--c[a[j]];
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		a[i] = b[i];
+	}
+	delete[]c;
+	delete[]b;
+}
+
+
+//基数排序 最大值小于等于9 按位排序 O(n+k) [因为用的计数排序是稳定排序]
+void radixSort(int* a, const int d, const int n)
+{
+	int* tmp = new int[n];
+	int* count = new int[10]; //计数器
+	int radix = 1;
+	int k = 0;
+	for (int i = 0; i < d; i++) //进行d次排序
+	{
+		for (int j = 0; j < 10; j++)
+			count[j] = 0; //每次分配前清空计数器
+		for (int j = 0; j < n; j++)
+		{
+			k = (a[j] / radix) % 10; //统计每个桶中的记录数
+			count[k]++;
+		}
+		for (int j = 1; j < 10; j++)
+			count[j] = count[j - 1] + count[j]; //将tmp中的位置依次分配给每个桶
+		for (int j = n - 1; j >= 0; j--) //将所有桶中记录依次收集到tmp中
+		{
+			k = (a[j] / radix) % 10;
+			tmp[count[k] - 1] = a[j];
+			count[k]--;
+		}
+		for (int j = 0; j < n; j++) //将临时数组的内容复制到data中
+			a[j] = tmp[j];
+		radix = radix * 10;
+	}
+	delete[]tmp;
+	delete[]count;
+}
+
+//桶排序 O(n) 因为对输入做了假设所以快
+//假设输入由随机过程产生，元素被均匀、独立地分布在[0,1)区间上
+//即每个元素 0<= x < n
+void bucketSort(int* a, const int n)
+{
+	int* b = new int[n];
+	for (int i = 0; i < n; ++i)
+	{
+		//make b[i] an empty DoubleList
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		//insert a[i] into DoubleList b[⌊na[i]⌋]
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		//sort DoubleList b[i] with insertion sort
+	}
+	//concatenate the DoubleLists b together in order
+}
+
+/*最坏情况为O(n)的选择算法*/
+//P123
+void Select()
+{
+
+}
+
+//基于数组实现的栈
+struct Stack
+{
+	int* data;
+	int top;
+	int maxSize;
+};
+
+void initStack(Stack& s)
+{
+	s.data = new int[SIZE];
+	s.top = -1;
+	s.maxSize = SIZE;
+}
+
+bool stackEmpty(Stack& s)
+{
+	return s.top < 0 ? true : false;
+}
+
+bool stackFull(Stack& s)
+{
+	return s.top == s.maxSize ? true : false;
+}
+
+void push(Stack& s, const int& n)
+{
+	if (stackFull(s))
+	{
+		exit(1);
+	}
+	else
+	{
+		s.data[++s.top] = n;
+	}
+}
+
+int pop(Stack& s)
+{
+	if (stackEmpty(s))
+	{
+		exit(2);
+	}
+	else
+	{
+		return s.data[s.top--];
+	}
+}
+
+//基于单链表实现的栈
+//分成两个结构体，单个太浪费空间
+struct ListStack
+{
+public:
+	bool isEmptyListStack()
+	{
+		if (!top)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	//链栈不必判满，只要能分配内存空间就可以
+	//入栈用头插法，因为出栈逆序
+	void pushListStack(const int& n)
+	{
+		ListStack* p = new ListStack;
+		p->data = n;
+		p->next = top;
+		top = p;
+	}
+	int popListStack()
+	{
+		if (!isEmptyListStack())
+		{
+			ListStack* r = top;
+			int n = top->data;
+			top = top->next;
+			delete r;
+			return n;
+		}
+		else
+		{
+			exit(2);
+		}
+	}
+private:
+	int data = 0;
+	ListStack* next = nullptr;
+	ListStack* top = nullptr;
+};
+
+//基于数组实现的循环队列
+//循环队列空出一个位置
+//循环队列满了后不能用值覆盖
+struct Queue
+{
+	int* data;
+	int head;
+	int tail;
+	int maxSize;
+	int length;
+};
+
+void initQueue(Queue& q)
+{
+	q.data = new int[SIZE];
+	q.maxSize = SIZE;
+	q.head = 0;
+	q.tail = 0;
+}
+
+bool queueEmpty(Queue& q)
+{
+	return q.head == q.tail ? true : false;
+}
+
+bool queueFull(Queue& q)
+{
+	return (q.tail + 1) % q.maxSize == q.head ? true : false;
+}
+
+void enQueue(Queue& q, const int& n)
+{
+	if (!queueFull(q))
+	{
+		q.data[q.tail] = n;
+		q.tail = (q.tail + 1) % q.maxSize;
+	}
+	else
+	{
+		exit(1);
+	}
+}
+
+int deQueue(Queue& q)
+{
+	if (!queueEmpty(q))
+	{
+		int x = q.data[q.head];
+		q.head = (q.head + 1) % q.maxSize;
+		return x;
+	}
+	else
+	{
+		exit(2);
+	}
+}
+
+//基于单链表实现的队列
+//分成两个结构体，单个太浪费空间
+struct ListQueue
+{
+	int data = 0, size = 0;
+	ListQueue* next = nullptr, * head = nullptr, * tail = nullptr;
+};
+
+bool isListQueueEmpty(ListQueue* l)
+{
+	return l->size == 0 ? true : false;
+}
+
+void enListQueue(ListQueue* l, const int& n)
+{
+	if (l->size == 0)
+	{
+		ListQueue* q = new ListQueue;
+		q->data = n;
+		l->head = q;
+		l->tail = q;
+		++l->size;
+	}
+	else
+	{
+		ListQueue* q = new ListQueue;
+		q->data = n;
+		q->next = nullptr;
+		l->tail->next = q;
+		l->tail = q;
+		++l->size;
+	}
+}
+
+int deListQueue(ListQueue* l)
+{
+	if (!isListQueueEmpty(l))
+	{
+		int n = l->head->data;
+		ListQueue* r = l->head;
+		l->head = l->head->next;
+		delete r;
+		--l->size;
+		return n;
+	}
+	else
+	{
+		exit(1);
+	}
+}
+
+//二叉树
+struct BiTree
+{
+	int data;
+	BiTree* left, * right;
+};
+
+void initBiTree(BiTree* t, const int& n)
+{
+	t->data = n;
+	t->left = nullptr;
+	t->right = nullptr;
+}
+BiTree* createBiTree(BiTree* t)
+{
+	int c;
+	cin >> c;
+	if (c == -1)
+	{
+		t = nullptr;
+	}
+	else
+	{
+		t = new BiTree;
+		t->data = c;
+		t->left = nullptr;
+		t->right = nullptr;
+		t->left = createBiTree(t->left);
+		t->right = createBiTree(t->right);
+	}
+	return t;
+}
+
+void xianXuPrint(BiTree* t)
+{
+	if (t != nullptr)
+	{
+		cout << t->data << " ";
+		xianXuPrint(t->left);
+		xianXuPrint(t->right);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	//多线程
+
+	//cout << argc << " " << argv[0];
+
+	//函数指针
+	//void (*p)(const int*, const int&) = print;
+
+	//rdbuf()获得一个指向当前流指针
+	/*std::ofstream log("./log.txt");
+	std::clog.rdbuf(log.rdbuf());
+	std::clog << "clog";*/
+
+	/*int a[2] = { 0 }, b[2] = { 1 };
+	cs(a, b);*/
+
+	//排序
+	/*int a[6] = { 5,2,4,6,1,3 };
+	int b[6] = { 0 };
+	int c[9] = { -2,1,-3,4,-1,2,1,-5,4 };
+	Maxarray m = find_MaxArray(c, 0, 8);
+	cout << m.getSum();*/
+	//print(a, 6);
+
+	/*print(a,6);
+	insert_Sort(a, 6);
+	select_Sort(a, 6);
+	maoPao(a, 6);
+	merge_Sort(a, 0, 5, b);
+	print(b, 6);*/
+
+	//int a[4] = { 1,1,0,1 };
+	//int b[4] = { 0,1,0,1 };
+	//add_Bin(a, b, 4);
+
+	/*Matrix a(3), b(3), c;
+	c = mulitiply(a, b);
+	c.print();*/
+
+	/*int a[6] = { 1,2,3,4,5,6 };
+	randomArray(a, 6);?
+	randomInPlace(a,6);?
+	print(a, 6);*/
+
+	/*Heap h;
+	h.a = new int [SIZE] { 5, 2, 4, 6, 1, 3 };
+	heapSort(h);
+	print(h.a, 6);*/
+
+	/*Heap h;
+	h.a = new int[SIZE] { 5, 2, 4, 6, 1, 3 };
+	buildMaxHeap(h);
+	cout << heapMax(h) << endl;
+	heapIncreaseKey(h, 2, 20);
+	cout << heapMax(h) << endl;
+	cout << heapExtractMax(h) << endl;
+	cout << heapMax(h) << endl;
+	maxHeapInsert(h, 4);
+	print(h.a, 7);
+	delete[]h.a;*/
+
+	/*int a[6] = { 5, 2, 4, 6, 1, 3 };
+	quickSort(a, 0, 5);
+	print(a, 6);*/
+
+	/*int a[6] = { 5, 2, 4, 6, 1, 3 };
+	randomQuickSort(a, 0, 5);
+	print(a, 6);*/
+
+	/*int a[6] = { 5, 2, 4, 6, 1, 3 };
+	countSort(a, 7, 6);
+	print(a, 6);*/
+
+	/*int a[6] = { 329, 457, 657, 839, 436, 720 };
+	radixSort(a,3,6);
+	print(a, 6);*/
+
+	//int a[5] = { 1, 3, 6, 6, 1};
+	//cout << minMaxValue(a, 5).min << " " << minMaxValue(a, 5).max << endl;
+
+	/*int a[6] = { 5, 2, 4, 6, 1, 3 };
+	cout << randomSelect(a, 1, 5, 6, 6);*/
+
+	/*Stack s;
+	initStack(s);
+	push(s, 1);
+	push(s, 2);
+	push(s, 3);
+	int a = pop(s);
+	int b = pop(s);
+	int c = pop(s);
+	std::cout << a << " " << b << " " << c << std::endl;*/
+
+	/*Queue q;
+	initQueue(q);
+	for (int i = 0; i < q.maxSize - 1; ++i)
+	{
+		enQueue(q, i);
+	}
+	for (int i = 0; i < q.maxSize - 1; ++i)
+	{
+		std::cout << deQueue(q) << " ";
+	}*/
+
+	/*ListStack l;
+	l.pushListStack(1);
+	l.pushListStack(2);
+	l.pushListStack(3);
+	std::cout << l.popListStack() << " " << l.popListStack() << " " << l.popListStack();*/
+
+	//连续输出倒序？
+	/*ListQueue* l = new ListQueue;
+	enListQueue(l, 1);
+	enListQueue(l, 2);
+	enListQueue(l, 3);
+	int a = deListQueue(l);
+	int b = deListQueue(l);
+	int c = deListQueue(l);
+	std::cout << a << " " << b << " " << c << std::endl;*/
+
+	/*BiTree* root = new BiTree;
+	root = createBiTree(root);
+	xianXuPrint(root);*/
+}
---
+++ sf-2.cpp
@@ -0,0 +1,591 @@
+﻿#include <iostream>
+#include <thread>
+#include <limits>
+#include <Windows.h>
+#include<sql.h>
+#include<sqlext.h>
+#include<sqltypes.h>
+#include <fstream>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+//双向链表
+struct DoubleList
+{
+	int data;
+	DoubleList* next, * prev;
+};
+
+//不带头结点的链表在处理插入/删除表头结点时要额外判断
+//带头结点会使其代码更简洁，但会浪费部分空间
+void initList(DoubleList* l)//尾插法(与输入顺序相同)
+{
+	bool flag = true;
+	int n = 0;
+	DoubleList* r, * head = new DoubleList;
+	head->prev = l;
+	head->next = nullptr;
+	l->prev = nullptr;
+	l->next = head;
+	r = head;
+	while (flag)
+	{
+		if (n != -1)
+		{
+			std::cin >> n;
+			DoubleList* s = new DoubleList;
+			s->data = n;
+			s->next = nullptr;
+			r->next = s;
+			s->prev = r;
+			r = r->next;
+		}
+		else
+			flag = false;
+	}
+}
+
+void insertList(DoubleList* l, const int& n, const int& x)
+{
+	l = l->next;
+	for (int i = 0; i < n - 1; ++i)
+		l = l->next;
+	DoubleList* q = new DoubleList;
+	q->data = x;
+	q->next = l->next;
+	l->next->prev = q;
+	l->next = q;
+	q->prev = l;
+}
+
+void deleteList(DoubleList* l, const int& n)
+{
+	l = l->next;
+	for (int i = 0; i < n - 1; ++i)
+		l = l->next;
+	DoubleList* s = l->next;
+	l->next = s->next;
+	s->next->prev = l;
+	delete s;
+}
+
+DoubleList* searchList(DoubleList* l, const int& n)
+{
+	while (l->next && l->data != n)
+		l = l->next;
+	return l;
+}
+
+void printList(DoubleList* l)
+{
+	l = l->next->next;
+	while (l->next)
+	{
+		std::cout << l->data << " ";
+		l = l->next;
+	}
+	std::cout << std::endl;
+}
+
+
+//直接寻址表
+struct Address
+{
+	int key;
+	int weiXingShuJu;
+};
+
+Address* directAddressSearch(Address** d, int& k)
+{
+	return d[k];
+}
+
+void directAddressInsert(Address** T, Address* x)
+{
+	T[x->key] = x;
+}
+
+void directAddressDelete(Address** T, Address* x)
+{
+	T[x->key] = nullptr;
+}
+
+//除法散列法
+int chuHash(const int& key, const int& m)
+{
+	return key % m;
+}
+//乘法散列法
+//a为依据Knuth所获得的常数,floor为向下取整
+int chengHash(const size_t& key, const size_t& m, const size_t& a)
+{
+	return floor(m * (key * a - floor(key * a)));
+}
+//散列表(链接法解决冲突)
+
+struct Data
+{
+	int key;
+	int data;
+};
+
+void hashInsert(DoubleList** l, Data& d, int* n)
+{
+	int k = chuHash(d.key, 10);
+	if (n[k] == 0)
+	{
+		l[k] = new DoubleList;
+		initList(l[k]);
+	}
+	insertList(l[k], 1, d.key);
+	++n[k];
+}
+
+DoubleList* hashSearch(DoubleList** l, const int& key)
+{
+	int k = chuHash(key, 10);
+	return searchList(l[k], key);
+}
+
+void hashDelete(DoubleList** l, Data& d)
+{
+	DoubleList* s = hashSearch(l, d.key);
+	s->prev->next = s->next;
+	s->next->prev = s->prev;
+	delete s;
+}
+
+//二叉搜索树
+struct Node
+{
+	int key;
+	Node* l, * r, * p;
+};
+
+class BiSearchTree
+{
+public:
+	BiSearchTree();
+	~BiSearchTree() = default;
+	void insert(const int& key);
+	void inOrder(Node* x);
+	void deleteSearch(const int& key);
+	//用以v为根节点的子树替换以u为根节点的子树
+	void transplant(Node* u, Node* v);
+	Node* search(const int& key);
+	Node* maxNode(Node* x);
+	Node* minNode(Node* x);
+	//寻找结点前驱(中序遍历)
+	Node* predecessor(Node* x);
+	//寻找结点后继(中序遍历)
+	Node* successor(Node* x);
+	Node* getRoot();
+private:
+	Node* root;
+};
+
+BiSearchTree::BiSearchTree()
+{
+	root = new Node;
+	cout << "root:";
+	cin >> root->key;
+	root->p = nullptr;
+	root->l = nullptr;
+	root->r = nullptr;
+}
+
+void BiSearchTree::insert(const int& key)
+{
+	Node* n = new Node;
+	n->key = key;
+	n->l = nullptr;
+	n->r = nullptr;
+	n->p = nullptr;
+	Node* x = root;
+	Node* y = nullptr;
+	while (x)
+	{
+		y = x;
+		if (n->key < x->key)
+		{
+			x = x->l;
+		}
+		else
+		{
+			x = x->r;
+		}
+	}
+	n->p = y;
+	if (!y)
+	{
+		root = n;
+	}
+	else if (n->key < y->key)
+	{
+		y->l = n;
+	}
+	else
+	{
+		y->r = n;
+	}
+}
+
+void BiSearchTree::inOrder(Node* x)
+{
+	if (x != nullptr)
+	{
+		inOrder(x->l);
+		cout << x->key << " ";
+		inOrder(x->r);
+	}
+}
+
+void BiSearchTree::deleteSearch(const int& key)
+{
+	Node* x = search(key);
+	if (!x->l)
+	{
+		transplant(x, x->r);
+	}
+	else if (!x->r)
+	{
+		transplant(x, x->l);
+	}
+	else
+	{
+		Node* y = minNode(x->r);
+		if (y->p != x)
+		{
+			transplant(y, y->r);
+			y->r = x->r;
+			y->r->p = y;
+		}
+		transplant(x, y);
+		y->l = x->l;
+		y->l->p = y;
+	}
+}
+
+Node* BiSearchTree::minNode(Node* x)
+{
+	while (x->l)
+	{
+		x = x->l;
+	}
+	return x;
+}
+
+Node* BiSearchTree::maxNode(Node* x)
+{
+	while (x->r)
+	{
+		x = x->r;
+	}
+	return x;
+}
+
+Node* BiSearchTree::predecessor(Node* x)
+{
+	if (x->l)
+	{
+		return maxNode(x->l);
+	}
+	Node* y = x->p;
+	while (y && x == y->l)
+	{
+		x = y;
+		y = y->p;
+	}
+	return y;
+}
+
+Node* BiSearchTree::successor(Node* x)
+{
+	if (x->r)
+		return minNode(x->r);
+	Node* y = x->p;
+	while (y && x == y->r)
+	{
+		x = y;
+		y = y->p;
+	}
+	return y;
+}
+
+void BiSearchTree::transplant(Node* u, Node* v)
+{
+	if (!u->p)
+	{
+		root = v;
+	}
+	else if (u == u->p->l)
+	{
+		u->p->l = v;
+	}
+	else
+	{
+		u->p->r = v;
+	}
+	if (v)
+		v->p = u->p;
+}
+
+Node* BiSearchTree::search(const int& key)
+{
+	Node* x = root;
+	while (x)
+	{
+		if (key < x->key)
+		{
+			x = x->l;
+		}
+		else if (key > x->key)
+		{
+			x = x->r;
+		}
+		else
+		{
+			return x;
+		}
+	}
+	return x;
+}
+
+Node* BiSearchTree::getRoot()
+{
+	return root;
+}
+
+//字典树 基数树
+
+//红黑树
+enum colorSet {
+	Red, Black
+};
+
+struct RBNode
+{
+	RBNode* p, * l, * r;
+	int color, key;
+};
+
+class RBTree
+{
+public:
+	RBTree();
+	~RBTree() = default;
+	void leftRotate(RBNode* x);
+	void rightRotate(RBNode* y);
+	void rbInsert(RBNode* z);
+	void rbInsertFixup(RBNode* z);
+private:
+	RBNode* root, * tnull;
+};
+
+RBTree::RBTree()
+{
+	root = new RBNode;
+	cout << "root:";
+	cin >> root->key;
+	root->color = Black;
+	root->p = nullptr;
+	root->l = nullptr;
+	root->r = nullptr;
+	tnull->color = Black;
+}
+
+void RBTree::leftRotate(RBNode* x)
+{
+	RBNode* y = x->r;
+	x->r = y->l;
+	if (y->l != tnull)
+	{
+		y->l->p = x;
+	}
+	y->p = x->p;
+	if (x->p == tnull)
+	{
+		root = y;
+	}
+	else if (x == x->p->l)
+	{
+		x->p->l = y;
+	}
+	else
+	{
+		x->p->r = y;
+	}
+	y->l = x;
+	x->p = y;
+}
+
+void RBTree::rightRotate(RBNode* y)
+{
+	RBNode* x = y->l;
+	y->l = x->r;
+	if (x->r != tnull)
+	{
+		x->r->p = y;
+	}
+	x->p = y->p;
+	if (y->p == tnull)
+	{
+		root = x;
+	}
+	else if (y == y->p->l)
+	{
+		y->p->l = x;
+	}
+	else
+	{
+		y->p->r = x;
+	}
+	x->r = y;
+	y->p = x;
+}
+
+//假设z的key值已经被赋值
+void RBTree::rbInsert(RBNode* z)
+{
+	RBNode* y = tnull;
+	RBNode* x = root;
+	while (x != tnull)
+	{
+		y = x;
+		if (z->key < x->key)
+		{
+			x = x->l;
+		}
+		else
+		{
+			x = x->r;
+		}
+	}
+	z->p = y;
+	if (y == tnull)
+	{
+		root = z;
+	}
+	else if (z->key < y->key)
+	{
+		y->l = z;
+	}
+	else
+	{
+		y->r = z;
+	}
+	//保持红黑树的结构
+	z->l = tnull;
+	z->r = tnull;
+	z->color = Red;
+	rbInsertFixup(z);
+}
+
+//保持红黑性质，因为插入点为红色结点
+void RBTree::rbInsertFixup(RBNode* z)
+{
+	while (z->p->color == Red)
+	{
+		if (z->p == z->p->p->l)
+		{
+			RBNode* y = z->p->p->r;
+			if (y->color == Red)
+			{
+				z->p->color = Black;
+				y->color = Black;
+				z->p->p->color = Red;
+				z = z->p->p;
+			}
+			else
+			{
+				if (z == z->p->r)
+				{
+					z = z->p;
+					leftRotate(z);
+				}
+				z->p->color = Black;
+				z->p->p->color = Red;
+				rightRotate(z->p->p);
+			}
+		}
+		else
+		{
+			RBNode* y = z->p->p->l;
+			if (y->color == Red)
+			{
+				z->p->color = Black;
+				y->color = Black;
+				z->p->p->color = Red;
+				z = z->p->p;
+			}
+			else
+			{
+				if (z == z->p->l)
+				{
+					z = z->p;
+					rightRotate(z);
+				}
+				z->p->color = Black;
+				z->p->p->color = Red;
+				leftRotate(z->p->p);
+			}
+		}
+	}
+	root->color = Black;
+}
+
+//非类型模板参数(需要常量表达式的地方可以使用非类型参数)
+//该参数必须具有静态生存期且是一个常量值,因为编译器必须在编译期间就能确认
+template<int N, int M>inline constexpr
+void cs(int(&a)[N], int(&b)[M])
+{
+	std::cout << a[0] << " " << b[0];
+}
+
+int main(int argc, char* argv[])
+{
+	/*DoubleList* l = new DoubleList;
+	initList(l);
+	printList(l);
+	insertList(l, 3, 100);
+	printList(l);
+	deleteList(l, 3);
+	printList(l);
+	DoubleList* sl = searchList(l, 5);
+	std::cout << sl->data;*/
+
+	/*DoubleList** l = new DoubleList * [10];
+	Data d1, d2, d3;
+	int list[10] = { 0 };
+	d1.key = 3;
+	d2.key = 13;
+	d3.key = 7;
+	hashInsert(l, d1,list);
+	hashInsert(l, d2, list);
+	hashInsert(l, d3, list);
+	cout << hashSearch(l, 13)->data << endl;
+	printList(l[3]);
+	hashDelete(l, d2);
+	cout << hashSearch(l, 13)->data << endl;*/
+
+	/*BiSearchTree b;
+	b.insert(6);
+	b.insert(18);
+	b.insert(3);
+	b.insert(7);
+	b.insert(17);
+	b.inOrder(b.getRoot());
+	cout << endl << b.search(7)->key << endl;
+	cout << b.predecessor(b.search(7))->key << endl;
+	cout << b.successor(b.search(7))->key << endl;
+	cout << b.minNode(b.getRoot())->key << " " << b.maxNode(b.getRoot())->key << endl;
+	b.deleteSearch(6);
+	b.inOrder(b.getRoot());*/
+
+
+
+	return 0;
+}
